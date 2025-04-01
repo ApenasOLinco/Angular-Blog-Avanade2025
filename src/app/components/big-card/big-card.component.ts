4228caf4747ea2cfba859cb92ae267e62aeffa21
ApenasOLinco <lincoln.matos1003@gmail.com>
2025-04-01 18:23:50 -0300

    Added dynamic properties in the card components

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, input, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-big-card',
-  imports: [],
-  templateUrl: './big-card.component.html',
-  styleUrl: './big-card.component.css'
+	selector: 'app-big-card',
+	imports: [],
+	templateUrl: './big-card.component.html',
+	styleUrl: './big-card.component.css',
 })
 export class BigCardComponent {
+	@Input()
+	photoCover: string = '';
 
+	@Input()
+	title: string = '';
+
+	@Input()
+	description: string = '';
 }
